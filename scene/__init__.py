3ea2e21c245e5be470093474fcdb4e82d0915939
BabackE <belmieh@gmail.com>
2026-05-20 14:18:26 -0700

    stage 2 - os pathfile check for colmap sparse files present

--- scene/__init__.py
+++ scene/__init__.py
@@ -42,7 +42,10 @@ class Scene:
         self.train_cameras = {}
         self.test_cameras = {}
 
-        if os.path.exists(os.path.join(args.source_path, "sparse")):
+        if os.path.exists(os.path.join(args.source_path, "data_COLMAP/000000/sparse")):
+            print("Found PKU-DyMvHumans COLMAP data, assuming PKU-DyMvHumans data set!")
+            scene_info = sceneLoadTypeCallbacks["PKUDyMvHumans"](args)
+        elif os.path.exists(os.path.join(args.source_path, "sparse")):
             scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
